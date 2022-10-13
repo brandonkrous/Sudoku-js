0df82df2d75fbd7f1443c024dfd103dbff5644c3
ArgyleMonster <krou7172@vandals.uidaho.edu>
2022-10-12 18:05:54 -0700

    Move Cell class to its own file cell.js

---
+++ cell.js
@@ -0,0 +1,42 @@
+class Cell {
+    constructor(x_coord, y_coord, cells) {
+        this.x_coord = x_coord;
+        this.y_coord = y_coord;
+        this.cells = cells;
+        this.availableNums = [1,2,3,4,5,6,7,8,9];
+        self.triedNums = [];
+    }
+    tryPickNum() {
+        return
+    }
+    isValid(originCell, num) {
+        return
+    }
+    addNum(num) {
+        return
+    }
+    removeNum(num) {
+        return
+    }
+    notifyAdd(num) {
+        return
+    }
+    notifyRemove(num) {
+
+    }
+    topLeftCell() {
+        return
+    }
+    nextCell() {
+        return
+    }
+    previousCell() {
+        return
+    }
+    clearTried() {
+        return
+    }
+    undo() {
+        return
+    }
+}
